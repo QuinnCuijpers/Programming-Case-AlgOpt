07cecf3a5e5be1c46e219d7e7b63a9df8d7020f9
QuinnCuijpers <Quinn.Cuijpers@gmail.com>
2024-12-14 20:35:15 +0100

    removed unnecessary comments from files.py

--- files.py
+++ files.py
@@ -10,7 +10,6 @@ checks every file, so can take a while to run.
 """
 
 
-# O(1) should be as none of this depends on the input size
 @dataclass
 class State:
     pos_a: int
@@ -33,14 +32,12 @@ class State:
         return f"({self.pos_a}, {self.pos_b}) at time {self.t}"
 
 
-# O(n(n+m)) as it does a bfs (through adjacancy list) for all nodes
 def bfs_dist(adj_matrix) -> List[List[int]]:
     n: int = len(adj_matrix)
     dist_matrix: List[List[int]] = [[-1] * n for _ in range(n)]
 
     for start in range(n):
-        # BFS from node 'start'
-        queue = deque([(start, 0)])  # (current_node, distance)
+        queue = deque([(start, 0)])
         visited: List[bool] = [False] * n
         visited[start] = True
 
@@ -56,24 +53,20 @@ def bfs_dist(adj_matrix) -> List[List[int]]:
     return dist_matrix
 
 
-# O(T) as there are at most T steps in the path
 def backtrack(s: State) -> List[tuple[int, int]]:
-    # indexing by plus 1 to go back to 1 based output
     path: List[tuple[int, int]] = [(s.pos_a + 1, s.pos_b + 1)]
     curr: State = s
-    # need to reduce this by checking only every second move
     while curr.prev:
         prev: State = curr.prev
         path.append((prev.pos_a + 1, prev.pos_b + 1))
         curr = prev
 
-    # all even pos in the path
     if len(path) % 2 == 1:
         path = [pos for i, pos in enumerate(path) if i % 2 == 0]
     else:
         path = [pos for i, pos in enumerate(path) if i % 2 == 1]
 
-    return path[::-1]  # O(n) for reversal of list
+    return path[::-1]
 
 
 def check(value, path, file) -> bool:
@@ -86,7 +79,6 @@ def check(value, path, file) -> bool:
     if value != true_value:
         return False
 
-    # decrementation happens to go back to 0 indexing
     for a, b in path:
         a -= 1
         b -= 1
@@ -95,7 +87,6 @@ def check(value, path, file) -> bool:
     return True
 
 
-# O(n+O(GN))
 def bfs(adj: List[List[int]], dist: List[List[int]], state: State, t_a: int, t_b: int):
 
     queue: Deque[State] = deque()
@@ -167,7 +158,6 @@ if __name__ == "__main__":
             t_b -= 1
             for _ in range(m):
                 a, b = tuple(map(int, file.readline().split(" ")))
-                # to go from 1 based indexing to 0 based
                 a -= 1
                 b -= 1
 
@@ -180,15 +170,6 @@ if __name__ == "__main__":
 
         time_end: float = perf_counter()
         time: float = time_end - time_begin
-
-        # if found:
-        #     print(steps)
-        #     print(" ".join(str(t[0]) for t in path))
-        #     print(" ".join(str(t[1]) for t in path))
-        #     print(f"{time:.2f} seconds")
-        # else:
-        #     print(T + 1)
-        #     print(f"{time:.2f} seconds")
 
         if found:
             if not check(steps, path, file.name):
